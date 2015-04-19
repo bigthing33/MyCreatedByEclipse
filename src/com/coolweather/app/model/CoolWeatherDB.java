901ce4311510b55c4f62099b8e6b4af81336c2b0
Nicola <bigthings@foxmail.com>
2015-04-20 00:44:11 +0800

    update db and create model of province,city and county

---
+++ src/com/coolweather/app/model/CoolWeatherDB.java
@@ -0,0 +1,131 @@
+package com.coolweather.app.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.coolweather.app.db.CoolWeatherOpenHelper;
+
+public class CoolWeatherDB {
+	//数据库名
+	public static final String DB_NAME="cool_weather";
+	//数据版本
+	public static final int VERSIO=1;
+	private static CoolWeatherDB coolWeatherDB;
+	private SQLiteDatabase db;
+	/**
+	 * 构造方法私有化
+	 * @param context
+	 */
+	private CoolWeatherDB(Context context) {
+		CoolWeatherOpenHelper dbHelper=new CoolWeatherOpenHelper(context, DB_NAME, null, VERSIO);
+	}
+	//获取实例
+	public synchronized static CoolWeatherDB getInstance(Context context){
+		if(coolWeatherDB==null){
+			coolWeatherDB=new CoolWeatherDB(context);
+		}
+			return coolWeatherDB;
+	}
+	/**
+	 * Province将实例存入数据库中
+	 */
+	public void saveProvinve(Province province){
+		if(province!=null){
+			ContentValues values=new ContentValues();
+			values.put("province_name", province.getProvinceName());
+			values.put("province_code", province.getProvinceCode());
+			db.insert("Province", null, values);
+		}
+	}
+	/**
+	 * Province读取数据库中的数据
+	 */
+	public List<Province>loadProvince(){
+		List<Province>list=new ArrayList<Province>();
+		Cursor cursor=db.query("Province", null, null, null, null, null, null);
+		if(cursor.moveToNext()){
+			do{
+				Province province=new Province();
+				province.setId(cursor.getInt(cursor.getColumnIndex("id")));
+				province.setProvinceName(cursor.getString(cursor.getColumnIndex("province_name")));
+				province.setProvinceCode(cursor.getString(cursor.getColumnIndex("province_code")));
+				list.add(province);
+			}while(cursor.moveToNext());
+		}
+		if(cursor!=null){
+			cursor.close();
+		}
+		return list;
+		
+	}
+	/**
+	 * City将实例存入数据库中
+	 */
+	public void saveCity(City city){
+		if(city!=null){
+			ContentValues values=new ContentValues();
+			values.put("city_name", city.getCityName());
+			values.put("city_code", city.getCityCode());
+			db.insert("City", null, values);
+		}
+	}
+	/**
+	 * City读取数据库中的数据
+	 */
+	public List<City>loadCity(){
+		List<City>list=new ArrayList<City>();
+		Cursor cursor=db.query("City", null, null, null, null, null, null);
+		if(cursor.moveToNext()){
+			do{
+				City city=new City();
+				city.setId(cursor.getInt(cursor.getColumnIndex("id")));
+				city.setCityName(cursor.getString(cursor.getColumnIndex("city_name")));
+				city.setCityCode(cursor.getString(cursor.getColumnIndex("city_code")));
+				list.add(city);
+			}while(cursor.moveToNext());
+		}
+		if(cursor!=null){
+			cursor.close();
+		}
+		return list;
+		
+	}
+	/**
+	 * County将实例存入数据库中
+	 */
+	public void saveCounty(County county){
+		if(county!=null){
+			ContentValues values=new ContentValues();
+			values.put("county_name", county.getCountyName());
+			values.put("county_code", county.getCountyCode());
+			db.insert("County", null, values);
+		}
+	}
+	/**
+	 * County读取数据库中的数据
+	 */
+	public List<County>loadCounty(){
+		List<County>list=new ArrayList<County>();
+		Cursor cursor=db.query("County", null, null, null, null, null, null);
+		if(cursor.moveToNext()){
+			do{
+				County county=new County();
+				county.setId(cursor.getInt(cursor.getColumnIndex("id")));
+				county.setCountyName(cursor.getString(cursor.getColumnIndex("county_name")));
+				county.setCountyCode(cursor.getString(cursor.getColumnIndex("county_code")));
+				list.add(county);
+			}while(cursor.moveToNext());
+		}
+		if(cursor!=null){
+			cursor.close();
+		}
+		return list;
+		
+	}
+
+}
