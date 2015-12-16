cd3caf370dd77cbd1867115524641a62c3c7523c
bigthing33 <390038427@qq.com>
2015-12-16 10:49:23 +0800

    add contans

---
+++ src/com/example/testdemo/utils/Contans.java
@@ -0,0 +1,30 @@
+package com.example.testdemo.utils;
+
+public class Contans {
+	/**
+	 * Unknown network class
+	 */
+	public static final int NETWORK_CLASS_UNKNOWN = 0;
+
+	/**
+	 * wifi net work
+	 */
+	public static final int NETWORK_WIFI = 1;
+
+	/**
+	 * "2G" networks
+	 */
+	public static final int NETWORK_CLASS_2_G = 2;
+
+	/**
+	 * "3G" networks
+	 */
+	public static final int NETWORK_CLASS_3_G = 3;
+
+	/**
+	 * "4G" networks
+	 */
+	public static final int NETWORK_CLASS_4_G = 4;
+
+
+}
