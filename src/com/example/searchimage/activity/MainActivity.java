a2f21b66562dc9f6620467b19c424094dca2b93b
bigthing33 <390038427@qq.com>
2016-01-12 18:17:32 +0800

    add fragment

--- src/com/example/searchimage/activity/MainActivity.java
+++ src/com/example/searchimage/activity/MainActivity.java
@@ -1,18 +1,45 @@
 package com.example.searchimage.activity;
 
-import com.example.searchimage.R;
-import com.example.searchimage.R.layout;
 
-import android.app.Activity;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
+import android.support.v4.view.ViewPager;
+import android.widget.FrameLayout;
 
-public class MainActivity extends Activity {
+import com.example.searchimage.R;
+import com.example.searchimage.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+	private final static String TAG = MainActivity.class.getSimpleName();
+	private Context mContext=MainActivity.this;
+	private ViewPager mViewPager;
+	private FrameLayout subFragmentContainer;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		mViewPager = new ViewPager(mContext);
+		mViewPager.setId(R.id.viewPager_explore);
+		mViewPager.setOffscreenPageLimit(2);//设置缓存个数
 		setContentView(R.layout.activity_main);
+	}
+
+	@SuppressLint("NewApi")
+	@Override
+	public void initUI() {
+		subFragmentContainer=(FrameLayout) findViewById(R.id.subFragmentContainer);
+		subFragmentContainer.addView(mViewPager);
+		final android.app.FragmentManager fm = getFragmentManager();
+
+		new Handler().postDelayed(new Runnable() {
+
+			@Override
+			public void run() {
+				
+			}
+		}, 100);
+		
 	}
 }
